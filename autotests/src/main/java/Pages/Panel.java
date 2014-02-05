e857aa58d53a9bac8281d12ec02f77445dcf84d5
Kseniya Binas <kseniyanyga@gmail.com>
2014-02-05 22:31:34 +0200

    added empty class Panel

---
+++ autotests/src/main/java/Pages/Panel.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Pages;
+
+import java.util.List;
+
+/**
+ *
+ * @author Ксения
+ */
+public class Panel {
+    public void SwitchBack(){}
+    public void SwitchForward(){}
+    public void PopUp(int index){}
+    public boolean isPopUpOpened(int index){return false;
+}
+    List<String> getLinks(){return null;
+}
+    public int iconCount(){return 0;}
+        }
+    
+    
