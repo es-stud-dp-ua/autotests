bf98abe44c5576fc675abbbf117047c901ced7f6
Kseniya Binas <kseniyanyga@gmail.com>
2014-02-05 21:44:04 +0200

    added Calendar

--- autotests/src/main/java/Pages/Calendar.java
+++ autotests/src/main/java/Pages/Calendar.java
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -12,7 +12,7 @@ import org.openqa.selenium.support.FindBy;
 /**
  *
  * @author Ксения
- */
+ */    
 public class Calendar {
     
      @FindBy(css=".month")
