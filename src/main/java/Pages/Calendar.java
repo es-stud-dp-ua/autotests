63e7f703c7699568794f84e31c8b4cff4c315088
Kseniya Binas <kseniyanyga@gmail.com>
2014-02-06 23:10:38 +0200

    Added Calendar,empty class Panel

--- src/main/java/Pages/Calendar.java
+++ src/main/java/Pages/Calendar.java
@@ -1,30 +1,65 @@
-/*
+﻿/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
-package pages;
+package Pages;
+import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.junit.Test;
 /**
  *
  * @author Ксения
- */
+ */    
 public class Calendar {
     
      @FindBy(css=".month")
-    public WebElement month;
-     public String getCalendarTitle(){
+    	public WebElement month;
+     	public String getCalendarTitle(){
         String title;
-         title = month.findElement(By.cssSelector(".month")) .getText();
+        title = month.findElement(By.cssSelector(".month")) .getText();
         return title;
      }
-     public WebElement left;
-     public void Arrays(){
-     String click;
-     click = left.
-     }
+       @FindBy(css=".left")
+      public WebElement back;
+      public String SwitchBack(){
+         back.findElement(By.cssSelector(".left"));
+         back.click();
+         month.findElement(By.cssSelector(".month"));
+         return month.getText();
+         }
+      @FindBy(css=".right")
+      public WebElement right;
+      public String SwitchForward(){
+          right.findElement(By.cssSelector(".right")).click();
+          month.findElement(By.cssSelector(".month"));
+          return month.getText();
+      }
+     /* public WebElement table;
+      public String tableId ="calendarTable";
+            public void Testcount() throws Exception {
+                         table.findElement(By.id(tableId));
+			 List<WebElement> days = table.findElements(By.tagName("tr").cssSelector("#calendarTable"));
+		 if (days!=null)
+		 {
+			 System.out.println(days.size());
+		 }
+		 else
+		 {
+			 System.out.print("null");
+		 }	
+	 }*/
+            public WebElement table;
+            public String tableId ="calendarTable";
+            public int Testcount() throws Exception {
+                   table.findElement(By.id(tableId));
+                   List<WebElement> days = table.findElements(By.tagName("tr").cssSelector("#calendarTable"));
+		   if (days!=null)
+		   return days.size();
+         return 0;
+		 	 	
+	 }
+            
 }
